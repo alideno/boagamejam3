0a2251f3ccb83e78085916e3ca31a433e36c0dec
Ali Deniz Sözer <113902419+alideno@users.noreply.github.com>
2025-02-16 13:40:31 +0300

    v

--- main_menu.gd
+++ main_menu.gd
@@ -1,12 +1,16 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("in main menu")
-	get_tree().change_scene_to_file("res://board.tscn")
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://board.tscn")
