341f84c139a72c9dcc5a898264a55fc0467f2d04
Tunagoksal <tuna.goksal@ug.bilkent.edu.tr>
2025-02-16 15:54:51 +0300

    finalfinalpls

--- moves.gd
+++ moves.gd
@@ -1,20 +1,15 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+var pictures = ["res://stuff/moves2.png","res://stuff/moves.png","res://stuff/pic1.jpg","res://stuff/pic2.jpg"]
+var i = 1
+	
 func _input(event):
 	if Input.is_action_just_pressed("NextPage"):
-		$Sprite2D.texture = load("res://stuff/moves.png")
-	elif Input.is_action_just_pressed("PrevPage"):
-		$Sprite2D.texture = load("res://stuff/moves2.png")
+		$Sprite2D.texture = load(pictures[i])
+		i = (i + 1)% len(pictures) 
 	elif Input.is_action_just_pressed("Escape"):
 		get_tree().change_scene_to_file("res://main_menu.tscn")
 		
 
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
